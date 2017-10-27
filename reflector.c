b579de80bf06852db4ad4f0acf6afb0e3bdbb23c
sdowell <sdowell@umail.ucsb.edu>
2017-10-27 16:41:28 -0700

    Add command line options

--- reflector.c
+++ reflector.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <pcap.h>
+#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
+	char *victim_ip = "";
+	char *victim_eth = "";
+	char *relayer_ip = "";
+	char *relayer_eth = "";
+	char *interface = "eth0";
+	
+
+	static struct option long_options[] = {
+            {"victim-ip",     required_argument, NULL,  'a' },
+            {"victim-ethernet",  required_argument, NULL,  'b' },
+            {"interface", required_argument,       NULL,  'c'},
+            {"relayer-ip",  required_argument, NULL, 'd'},
+            {"relayer-ethernet",    required_argument, NULL,  'e' },
+            {0,         0,                 0,  0 }
+        };
+	while ((ch = getopt_long(argc, argv, "", long_options, NULL)) != -1)
+	{
+    		// check to see if a single character or long option came through
+   		switch (ch)
+    		{
+         	// short option 't'
+         	case 'a':
+             		victim_ip = optarg; // or copy it if you want to
+             		break;
+         		// short option 'a'
+         	case 'b':
+             		victim_eth = optarg; // or copy it if you want to
+             		break;
+		case 'c':
+			interface = optarg
+			break;
+		case 'd':
+			relayer_ip = optarg;
+			break;
+		case 'e':
+			relayer_eth = optarg;
+			break;
+    		}
+	}
+	printf("Victim_ip = %s, victim_eth = %s\n relayer_ip = %s, relayer_eth = %s\n device = %s\n", victim_ip, victim_eth, relayer_ip, relayer_eth, interface);
+	
 	char *dev, errbuf[PCAP_ERRBUF_SIZE];
 
 	dev = pcap_lookupdev(errbuf);
