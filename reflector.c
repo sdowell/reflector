7bd681d9396d6cf31cb1f0e935f99508763dccdf
sdowell <sdowell@umail.ucsb.edu>
2017-10-30 00:11:33 -0700

    Update reflector.c

--- reflector.c
+++ reflector.c
@@ -34,17 +34,17 @@
 	/* IP header */
 	struct sniff_ip {
 		u_char ip_vhl;		/* version << 4 | header length >> 2 */
-		u_char ip_tos;		/* type of service */
-		u_short ip_len;		/* total length */
-		u_short ip_id;		/* identification */
-		u_short ip_off;		/* fragment offset field */
+		u_int8_t ip_tos;		/* type of service */
+		u_int16_t ip_len;		/* total length */
+		u_int16_t ip_id;		/* identification */
+		u_int16_t ip_off;		/* fragment offset field */
 	#define IP_RF 0x8000		/* reserved fragment flag */
 	#define IP_DF 0x4000		/* dont fragment flag */
 	#define IP_MF 0x2000		/* more fragments flag */
 	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
-		u_char ip_ttl;		/* time to live */
-		u_char ip_p;		/* protocol */
-		u_short ip_sum;		/* checksum */
+		u_int8_t ip_ttl;		/* time to live */
+		u_int8_t ip_p;		/* protocol */
+		u_int16_t ip_sum;		/* checksum */
 		struct in_addr ip_src,ip_dst; /* source and dest address */
 	};
 	#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
