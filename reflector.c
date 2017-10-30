118d89fcb2cd2dd8a3ad65012419b260602220ae
sdowell <sdowell@umail.ucsb.edu>
2017-10-30 00:00:34 -0700

    Update reflector.c

--- reflector.c
+++ reflector.c
@@ -159,7 +159,7 @@ void relay_IP(const struct sniff_ethernet *ethernet, const struct sniff_ip *ip, 
 	libnet_clear_packet(ln_context);
 	if (libnet_build_ipv4 (ip->ip_len,
     		ip->ip_tos, ip->ip_id, ip->ip_off,
-    		ip->ip_ttl, ip->ip_p, ip->ip_sum,
+    		ip->ip_ttl, IPPROTO_ICMP, ip->ip_sum,
     		r_ip, ip->ip_src.s_addr, payload,
     		payload_s, ln_context, 0) == -1 )
   	{
